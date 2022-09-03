d86a967051315d0aced25d8e06d8311db8c20750
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-03 16:35:59 +0900

    Lecture_Practice

--- src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int val = 5;
-        int val_02 = 10;
-        System.out.println("Hello, World!");
-        int val_03 = 20;
-    }
-}
