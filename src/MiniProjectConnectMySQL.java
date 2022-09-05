8b24d4d42fd326fdfe0f05b3477af0dd45bd0fb0
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-05 09:45:32 +0900

    Lecture_Practice

--- src/MiniProjectConnectMySQL.java
+++ src/MiniProjectConnectMySQL.java
@@ -2,7 +2,7 @@ public class MiniProjectConnectMySQL {
     public static void main(String[] args) {
         // Process 1. MySQL 접속 -> Connect to Database 
                             //(Host,Username 확인 - 그리고 default schema로 'world' 선택) 
-        // Process 2. Process 1에서 Database 'world'에 Connect 되었는지 확인 (Bold)
+        // Process 2. Process 1에서 Database 'world'에 Connect 되었는지 확인 (Bold로 Highlight)
         // Process 3. CITY TABLE 에서 모든 DATA를 출력 
                         // SELECT * FROM CITY 쿼리문 입력, CTRL + ENTER로 실행
         // Process 4.  IF succeeded, 
